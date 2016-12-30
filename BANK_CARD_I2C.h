9e21f7b7523e3c27e74066ba49abe0ebd5f5d949
Dmitry7777 <list-dmitrij@mail.ru>
2016-12-30 11:58:55 +0300

    Update BANK_CARD_I2C.h

--- BANK_CARD_I2C.h
+++ BANK_CARD_I2C.h
@@ -1,3 +1,19 @@
 #ifndef BANK_CARD_I2C_H
 #define BANK_CARD_I2C_H
 
+#include<BANK_CARD_I2C.h> //
+#include<Arduino.h> //
+#include<BANK_CARD_I2C.h> //
+#include<Arduino.h> //
+
+class BANK_CARD_I2C{
+
+
+public:
+
+private:
+
+protected:
+
+  #endif;
+}
